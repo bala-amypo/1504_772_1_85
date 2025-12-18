93bb4fd100df03e33ed36a6065cd84e9c1ef0578
bala-amypo <bala@amypo.in>
2025-12-18 14:06:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/FacilityScore.java
@@ -0,0 +1,22 @@
+@Entity
+public class FacilityScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Property property;
+
+    @Max(10) @Min(0)
+    private Integer schoolProximity;
+
+    @Max(10) @Min(0)
+    private Integer hospitalProximity;
+
+    @Max(10) @Min(0)
+    private Integer transportAccess;
+
+    @Max(10) @Min(0)
+    private Integer safetyScore;
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,17 @@
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    private String role = "ANALYST";
+}
