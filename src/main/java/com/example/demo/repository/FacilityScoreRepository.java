c6daf51907cb01f8878aacffffa6331020669759
bala-amypo <bala@amypo.in>
2025-12-19 04:31:07 +0000

    User commit

--- src/main/java/com/example/demo/repository/FacilityScoreRepository.java
+++ src/main/java/com/example/demo/repository/FacilityScoreRepository.java
@@ -1,3 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.FacilityScore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 public interface FacilityScoreRepository extends JpaRepository<FacilityScore, Long> {
     Optional<FacilityScore> findByPropertyId(Long propertyId);
 }
--- src/main/java/com/example/demo/repository/PropertyRepository.java
+++ src/main/java/com/example/demo/repository/PropertyRepository.java
@@ -1,3 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 public interface PropertyRepository extends JpaRepository<Property, Long> {
 
     List<Property> findByCity(String city);
