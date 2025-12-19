de79db1ae212fa11db973725f15f5431cb2ff75f
bala-amypo <bala@amypo.in>
2025-12-19 04:13:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/RatingLogController.java
+++ src/main/java/com/example/demo/controller/RatingLogController.java
@@ -1,3 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.RatingLog;
+import com.example.demo.service.RatingLogService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/logs")
 @Tag(name = "Rating Logs")
