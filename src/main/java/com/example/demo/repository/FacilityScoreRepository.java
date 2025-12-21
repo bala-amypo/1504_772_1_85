2decbd88e1a4b1b3c4bcbbc3a3277024095083db
bala-amypo <bala@amypo.in>
2025-12-21 14:17:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/FacilityScoreRepository.java
+++ src/main/java/com/example/demo/repository/FacilityScoreRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.FacilityScore;
+import com.example.demo.entity.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface FacilityScoreRepository extends JpaRepository<FacilityScore, Long> {
-    Optional<FacilityScore> findByPropertyId(Long propertyId);
+    Optional<FacilityScore> findByProperty(Property property);
 }
