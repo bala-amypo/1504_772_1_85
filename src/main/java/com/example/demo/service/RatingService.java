4ed4fef0f82e210c6864a55dc2b05b7c6ffb017e
bala-amypo <bala@amypo.in>
2025-12-26 06:12:22 +0000

    User commit

--- src/main/java/com/example/demo/service/RatingService.java
+++ src/main/java/com/example/demo/service/RatingService.java
@@ -4,7 +4,5 @@ import com.example.demo.entity.RatingResult;
 
 public interface RatingService {
 
-    RatingResult generateRating(Long propertyId);
-
-    RatingResult getRating(Long propertyId);
+    RatingResult calculateRating(Long propertyId);
 }
